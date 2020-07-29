7f4b23a507193cd18a082d89e44538c7b30d594a
Zachary Wilson <wilsonzachary634@gmail.com>
2020-07-29 14:32:14 -0600

    terraform apply step

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "con-pca-stage-tf-backend"
-    key    = "con-pca-api.tfstate"
-    region = "us-east-1"
+    bucket         = "con-pca-stage-tf-backend"
+    key            = "con-pca-api.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "con-pca-tf-lock"
   }
 }
