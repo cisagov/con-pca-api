b48113eca21dc668869d8e2fe3615cbe5dd4c727
Zachary Wilson <wilsonzachary634@gmail.com>
2020-07-30 09:25:59 -0600

    tf setup for multiple environments

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,4 @@
 terraform {
   backend "s3" {
-    bucket         = "con-pca-stage-tf-backend"
-    key            = "con-pca-api.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "con-pca-tf-lock"
   }
 }
