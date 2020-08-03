609cd137463edf123dfce4b04e05c285de3c9491
Peter Mercado <peter.mercado255@gmail.com>
2020-08-03 12:27:41 -0400

    fixing date compair error

--- src/reports/views/cycle_view.py
+++ src/reports/views/cycle_view.py
@@ -324,7 +324,7 @@ class CycleStatusView(APIView):
     def get(self, request, **kwargs):
 
         start_date_param = self.kwargs["start_date"]
-        start_date = datetime.strptime(start_date_param, "%Y-%m-%dT%H:%M:%S.%f%z")
+        start_date = datetime.strptime(start_date_param, "%Y-%m-%dT%H:%M:%SZ")
 
         # Get targeted subscription and associated customer data
         subscription_uuid = self.kwargs["subscription_uuid"]
