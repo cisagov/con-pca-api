888bc281d4c0bf2b2f159b28eab907cd429faf4c
Zachary Wilson <wilsonzachary634@gmail.com>
2020-08-07 08:58:22 -0600

    revert sending profile

--- src/api/utils/subscription/actions.py
+++ src/api/utils/subscription/actions.py
@@ -3,8 +3,6 @@
 # Standard Python Libraries
 from datetime import datetime
 import logging
-import uuid
-import copy
 
 # Third-Party Libraries
 from api.manager import CampaignManager
@@ -12,6 +10,7 @@ from api.models.dhs_models import DHSContactModel, validate_dhs_contact
 from api.models.subscription_models import SubscriptionModel, validate_subscription
 from api.serializers.subscriptions_serializers import SubscriptionPatchSerializer
 from api.utils import db_utils as db
+from api.utils import webhooks
 from api.utils.customer.customers import get_customer
 from api.utils.subscription.campaigns import generate_campaigns, stop_campaign
 from api.utils.subscription.subscriptions import (
@@ -107,7 +106,7 @@ def start_subscription(data=None, subscription_uuid=None):
     # landing_template_list = get_list({"template_type": "Landing"}, "template", TemplateModel, validate_template)
     landing_page = "Phished"
 
-    deal_with_sending_profiles(subscription)
+    # webhooks.deal_with_sending_profiles(subscription)
 
     subscription["gophish_campaign_list"] = generate_campaigns(
         subscription, landing_page, sub_levels
@@ -155,76 +154,6 @@ def start_subscription(data=None, subscription_uuid=None):
     )
 
     return response
-
-
-def deal_with_sending_profiles(subscription):
-    # determine if we need to create a new default sending profile
-    # for this subscription or not
-    # the basic rule is each subscription should have only one sending profile
-    # if a sending profile ends with the subscription name then don't create a new one
-    # just update it with the new header guid
-    # else create a new one.
-    sending_profiles = campaign_manager.get("sending_profile")
-    sending_profile_name = subscription["sending_profile_name"]
-    new_header_value = str(uuid.uuid4())
-
-    if sending_profile_name.endswith(subscription["name"]):
-        # we are going to update it
-        sending_profile_to_clone = next(
-            iter([p for p in sending_profiles if p.name == sending_profile_name]), None
-        )
-    else:
-        sending_profile_to_clone = next(
-            iter([p for p in sending_profiles if p.name == sending_profile_name]), None
-        )
-        # check to make sure it is really not there
-        sending_profile_name = sending_profile_name + "_" + subscription["name"]
-        sending_profile_to_new_name = next(
-            iter([p for p in sending_profiles if p.name == sending_profile_name]), None
-        )
-        if sending_profile_to_new_name:
-            sending_profile_to_clone = sending_profile_to_new_name
-        else:
-            # ok it is really not there so clone it
-            # this is not necessary
-            sending_profile_to_clone = copy.deepcopy(sending_profile_to_clone)
-
-    # check to see if sending profile exists
-    # if so update it else create it.
-    # get the id of the sending profile we want
-
-    new_headers = None
-    if not sending_profile_to_clone.headers:
-        new_headers = [{"key": "DHS-PHISH", "value": new_header_value}]
-    else:
-        # update the header with the new value
-        header = next(
-            iter(
-                [h for h in sending_profile_to_clone.headers if h["key"] == "DHS-PHISH"]
-            ),
-            None,
-        )
-        header["value"] = new_header_value
-
-    if sending_profile_to_clone is None:
-        campaign_manager.create(
-            "sending_profile",
-            name=sending_profile_name,
-            username=sending_profile_to_clone.username,
-            password=sending_profile_to_clone.password,
-            host=sending_profile_to_clone.host,
-            interface_type=sending_profile_to_clone.interface_type,
-            from_address=sending_profile_to_clone.from_address,
-            ignore_cert_errors=sending_profile_to_clone.ignore_cert_errors,
-            headers=new_headers,
-        )
-    else:
-        campaign_manager.put_sending_profile(sending_profile_to_clone)
-
-    subscription["sending_profile_name"] = sending_profile_name
-    # clone the sending profile changing the header value
-    # create the new sending profile
-    # tell the new campaigns to and cycle to use that profile
 
 
 def new_subscription_cycle(subscription_uuid):
