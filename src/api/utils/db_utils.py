e4f0809ee2866f0ed6476ee6b2654a50f5b3a334
Zachary Wilson <wilsonzachary634@gmail.com>
2020-09-21 12:10:07 -0600

    tasks in lambda

--- src/api/utils/db_utils.py
+++ src/api/utils/db_utils.py
@@ -34,7 +34,7 @@ def __db_service(collection_name, model, validate_model):
 
 def get_mongo_uri():
     if os.environ.get("MONGO_TYPE", "MONGO") == "DOCUMENTDB":
-        mongo_uri = "mongodb://{}:{}@{}:{}/?ssl=true&ssl_ca_certs=/app/rds-combined-ca-bundle.pem&retryWrites=false".format(
+        mongo_uri = "mongodb://{}:{}@{}:{}/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&retryWrites=false".format(
             settings.DB_CONFIG["DB_USER"],
             settings.DB_CONFIG["DB_PW"],
             settings.DB_CONFIG["DB_HOST"],
