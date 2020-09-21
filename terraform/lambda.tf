a18290d2dce109e4338af278e35415c02e504c69
Zachary Wilson <wilsonzachary634@gmail.com>
2020-09-21 15:57:50 -0600

    add comma to layers

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -35,7 +35,7 @@ resource "aws_lambda_function" "tasks" {
   timeout          = 300
 
   layers = [
-    "arn:aws:lambda:us-east-1:668099181075:layer:AWSLambda-Python38-SciPy1x:29"
+    "arn:aws:lambda:us-east-1:668099181075:layer:AWSLambda-Python38-SciPy1x:29",
     aws_lambda_layer_version.layer.arn
   ]
 
