6ca28912fef84c1a86ac8b8eb2994b42d86eb806
Zachary Wilson <wilsonzachary634@gmail.com>
2020-09-21 14:58:44 -0600

    remove lambda layer

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,47 +1,47 @@
 # ===================================
 # Lambda Layers
 # ===================================
-data "archive_file" "django" {
-  type        = "zip"
-  source_dir  = "${path.module}/layers/django"
-  output_path = "${path.module}/output/django.zip"
-}
+# data "archive_file" "django" {
+#   type        = "zip"
+#   source_dir  = "${path.module}/layers/django"
+#   output_path = "${path.module}/output/django.zip"
+# }
 
-resource "aws_lambda_layer_version" "django" {
-  filename         = data.archive_file.django.output_path
-  source_code_hash = data.archive_file.django.output_path
-  layer_name       = "${var.app}-${var.env}-django"
+# resource "aws_lambda_layer_version" "django" {
+#   filename         = data.archive_file.django.output_path
+#   source_code_hash = data.archive_file.django.output_path
+#   layer_name       = "${var.app}-${var.env}-django"
 
-  compatible_runtimes = ["python3.8"]
-}
+#   compatible_runtimes = ["python3.8"]
+# }
 
-data "archive_file" "reports" {
-  type        = "zip"
-  source_dir  = "${path.module}/layers/reports"
-  output_path = "${path.module}/output/reports.zip"
-}
+# data "archive_file" "reports" {
+#   type        = "zip"
+#   source_dir  = "${path.module}/layers/reports"
+#   output_path = "${path.module}/output/reports.zip"
+# }
 
-resource "aws_lambda_layer_version" "reports" {
-  filename         = data.archive_file.reports.output_path
-  source_code_hash = data.archive_file.reports.output_path
-  layer_name       = "${var.app}-${var.env}-reports"
+# resource "aws_lambda_layer_version" "reports" {
+#   filename         = data.archive_file.reports.output_path
+#   source_code_hash = data.archive_file.reports.output_path
+#   layer_name       = "${var.app}-${var.env}-reports"
 
-  compatible_runtimes = ["python3.8"]
-}
+#   compatible_runtimes = ["python3.8"]
+# }
 
-data "archive_file" "other" {
-  type        = "zip"
-  source_dir  = "${path.module}/layers/other"
-  output_path = "${path.module}/output/other.zip"
-}
+# data "archive_file" "other" {
+#   type        = "zip"
+#   source_dir  = "${path.module}/layers/other"
+#   output_path = "${path.module}/output/other.zip"
+# }
 
-resource "aws_lambda_layer_version" "other" {
-  filename         = data.archive_file.other.output_path
-  source_code_hash = data.archive_file.other.output_path
-  layer_name       = "${var.app}-${var.env}-other"
+# resource "aws_lambda_layer_version" "other" {
+#   filename         = data.archive_file.other.output_path
+#   source_code_hash = data.archive_file.other.output_path
+#   layer_name       = "${var.app}-${var.env}-other"
 
-  compatible_runtimes = ["python3.8"]
-}
+#   compatible_runtimes = ["python3.8"]
+# }
 
 # ===================================
 # Lambda Function
@@ -62,11 +62,11 @@ resource "aws_lambda_function" "tasks" {
   source_code_hash = data.archive_file.code.output_base64sha256
   timeout          = 300
 
-  layers = [
-    aws_lambda_layer_version.other.arn,
-    aws_lambda_layer_version.django.arn,
-    aws_lambda_layer_version.reports.arn
-  ]
+  # layers = [
+  #   aws_lambda_layer_version.other.arn,
+  #   aws_lambda_layer_version.django.arn,
+  #   aws_lambda_layer_version.reports.arn
+  # ]
 
   environment {
     variables = local.environment
