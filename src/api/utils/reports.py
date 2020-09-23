7bcdd1d915f63a3009cd08e3b0f59d205c53d0ff
Zachary Wilson <wilsonzachary634@gmail.com>
2020-09-23 11:36:31 -0600

    suppress some asyncio warnings

--- src/api/utils/reports.py
+++ src/api/utils/reports.py
@@ -2,6 +2,7 @@ from io import BytesIO
 
 import pyppeteer
 import asyncio
+from contextlib import suppress
 
 from config import settings
 
@@ -16,6 +17,11 @@ def download_pdf(report_type, uuid, cycle, cycle_uuid=None):
             report_type, uuid, cycle, auth_header=auth_header, cycle_uuid=cycle_uuid
         )
     )
+
+    with suppress(asyncio.exceptions.CancelledError):
+        pending = asyncio.Task.all_tasks()
+        loop.run_until_complete(asyncio.gather(*pending))
+
     buffer = BytesIO()
     buffer.write(response)
     buffer.seek(0)
