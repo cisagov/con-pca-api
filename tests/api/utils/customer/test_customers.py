17c57e3cee22fb9de41a764a4ad901bfcee9f085
Zachary Wilson <wilsonzachary634@gmail.com>
2020-09-29 09:49:16 -0600

    pytest api/utils/customer

--- tests/api/utils/customer/test_customers.py
+++ tests/api/utils/customer/test_customers.py
@@ -1,9 +1,35 @@
+from unittest import mock
+from faker import Faker
+
 from src.api.utils.customer import customers
 
 
-def test_get_customer():
-    return
+@mock.patch("api.utils.db_utils.get_single")
+def test_get_customer(mocked_get):
+    assert mocked_get.assert_called
 
 
 def test_get_full_customer_address():
-    return
+    fake = Faker()
+    street_address = fake.street_address()
+    city = fake.city()
+    state = fake.state()
+    zip_code = fake.zipcode()
+
+    customer_info = {
+        "address_1": street_address,
+        "address_2": "",
+        "city": city,
+        "state": state,
+        "zip_code": zip_code,
+    }
+
+    full_address = customers.get_full_customer_address(customer_info)
+    assert full_address == f"{street_address}\n{city}\n{state}\n{zip_code}"
+
+    customer_info["address_2"] = street_address
+    full_address = customers.get_full_customer_address(customer_info)
+    assert (
+        full_address
+        == f"{street_address}\n{street_address}\n{city}\n{state}\n{zip_code}"
+    )
