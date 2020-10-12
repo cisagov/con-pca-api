a71516782381c7449dc90bc7c139c8970f044757
Zachary Wilson <wilsonzachary634@gmail.com>
2020-10-12 09:40:59 -0600

    moved terraform

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "inl-tf-backend"
-    key            = "con-pca-api"
-    region         = "us-east-1"
-    dynamodb_table = "inl-tf-lock"
-  }
-}
