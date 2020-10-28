5781718df3018a1a4cd7c06a5dc0577c55cab9a2
Zachary Wilson <wilsonzachary634@gmail.com>
2020-10-28 09:07:57 -0600

    optimize target history update

--- src/api/utils/template/templates.py
+++ src/api/utils/template/templates.py
@@ -15,33 +15,22 @@ def update_target_history(campaign_info, seralized_data):
         seralized_data (dict): seralized_data
     """
     # check if email target exists, if not, create
-    document_list = target_history_service.get_list({"email": seralized_data["email"]})
-    if document_list:
-        # If object exists, update with latest template info
-        target = document_list[0]
-        filter_target_history = list(
-            filter(
-                lambda x: x["template_uuid"] == campaign_info["template_uuid"],
-                target["history_list"],
-            )
+    data = {
+        "template_uuid": campaign_info["template_uuid"],
+        "sent_timestamp": format_ztime(seralized_data["time"]),
+    }
+
+    if target_history_service.exists({"email": seralized_data["email"]}):
+        target = target_history_service.get_list({"email": seralized_data["email"]})[0]
+        target_history_service.push_nested(
+            uuid=target["target_uuid"],
+            field="history_list",
+            data=data,
         )
-        if not filter_target_history:
-            target["history_list"].append(
-                {
-                    "template_uuid": campaign_info["template_uuid"],
-                    "sent_timestamp": format_ztime(seralized_data["time"]),
-                }
-            )
-            target_history_service.update(target["target_uuid"], target)
     else:
         # create new target history if not exisiting
-        targert_hist = {
+        target_hist = {
             "email": seralized_data["email"],
-            "history_list": [
-                {
-                    "template_uuid": campaign_info["template_uuid"],
-                    "sent_timestamp": format_ztime(seralized_data["time"]),
-                }
-            ],
+            "history_list": [data],
         }
-        target_history_service.save(targert_hist)
+        target_history_service.save(target_hist)
