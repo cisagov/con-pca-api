815969a3b6186c790dccc96a31a297bd673f3085
Zachary Wilson <wilsonzachary634@gmail.com>
2020-10-30 08:43:03 -0600

    Revert "Merge pull request #138 from cisagov/update/complexity"
    This reverts commit 785787450a8c7b464776b6569e579b37a9b0b321, reversing
    changes made to 09c700a33967acc660035c80a6c61bf344b87fd0.

--- src/reports/views/system_view.py
+++ src/reports/views/system_view.py
@@ -29,12 +29,30 @@ class SystemReportsView(APIView):
         sub_parameters = {"archived": {"$in": [False, None]}}
         subscriptions = subscription_service.get_list(sub_parameters)
 
-        timeline_status = self._get_timeline_list(subscriptions)
+        cycles_started = 0
+        monthly_reports_sent = 0
+        cycle_reports_sent = 0
+        yearly_reports_sent = 0
+
+        _timeline_list = []
+
+        for sub in subscriptions:
+            for email_notifcation in sub["email_report_history"]:
+                if email_notifcation["report_type"] == "Cycle Start Notification":
+                    cycles_started += 1
+                if email_notifcation["report_type"] == "Monthly":
+                    monthly_reports_sent += 1
+                if email_notifcation["report_type"] == "Cycle":
+                    cycle_reports_sent += 1
+                if email_notifcation["report_type"] == "Yearly":
+                    yearly_reports_sent += 1
+            for campaign in sub["campaigns"]:
+                _timeline_list.append(campaign["timeline"])
 
         timeline_item_summary = []
         all_stats = []
 
-        for timeline in timeline_status["timeline_list"]:
+        for timeline in _timeline_list:
             unique_moments = get_unique_moments(timeline)
             for unique_moment in unique_moments:
                 append_timeline_moment(unique_moment, timeline_item_summary)
@@ -50,90 +68,6 @@ class SystemReportsView(APIView):
         consolidated_stats = consolidate_campaign_group_stats(all_stats)
         consolidated_stats["ratios"] = calc_ratios(consolidated_stats)
 
-        customer_stats = self._get_customer_type_count()
-
-        gov_group_stats = get_gov_group_stats()
-        gov_group_stats["fed_stats"]["customer_count"] = customer_stats[
-            "federal_customers"
-        ]
-        gov_group_stats["state_stats"]["customer_count"] = customer_stats[
-            "state_customers"
-        ]
-        gov_group_stats["local_stats"]["customer_count"] = customer_stats[
-            "local_customers"
-        ]
-        gov_group_stats["tribal_stats"]["customer_count"] = customer_stats[
-            "tribal_customers"
-        ]
-        gov_group_stats["private_stats"]["customer_count"] = customer_stats[
-            "private_customers"
-        ]
-
-        if "average" in consolidated_stats["clicked"]:
-            avgTimeToClick = format_timedelta(consolidated_stats["clicked"]["average"])
-        else:
-            avgTimeToClick = None
-
-        context = {
-            "customers_enrolled": customer_stats["customers_enrolled"],
-            "monthly_reports_sent": timeline_status["monthly_reports_sent"],
-            "cycle_reports_sent": timeline_status["cycle_reports_sent"],
-            "yearly_reports_sent": timeline_status["yearly_reports_sent"],
-            "federal_stats": gov_group_stats["fed_stats"],
-            "state_stats": gov_group_stats["state_stats"],
-            "local_stats": gov_group_stats["local_stats"],
-            "tribal_territorial_stats": gov_group_stats["tribal_stats"],
-            "critical_infrastructure_private_stats": gov_group_stats["private_stats"],
-            "click_rate_across_all_customers": consolidated_stats["ratios"][
-                "clicked_ratio"
-            ],
-            "average_time_to_click_all_customers": consolidated_stats["clicked"],
-            "avgTimeToClick": avgTimeToClick
-            # ["clicked"]["average"],
-        }
-
-        return Response(context, status=status.HTTP_202_ACCEPTED)
-
-    def _get_timeline_list(self, subscriptions):
-        """Get_timeline_list.
-
-        Args:
-            subscriptions (subscription): subscriptions object
-
-        Returns:
-            dict: returns dict holding timeline states and list
-        """
-        cycles_started = 0
-        monthly_reports_sent = 0
-        cycle_reports_sent = 0
-        yearly_reports_sent = 0
-        timeline_list = []
-        for sub in subscriptions:
-            for email_notifcation in sub["email_report_history"]:
-                if email_notifcation["report_type"] == "Cycle Start Notification":
-                    cycles_started += 1
-                if email_notifcation["report_type"] == "Monthly":
-                    monthly_reports_sent += 1
-                if email_notifcation["report_type"] == "Cycle":
-                    cycle_reports_sent += 1
-                if email_notifcation["report_type"] == "Yearly":
-                    yearly_reports_sent += 1
-            for campaign in sub["campaigns"]:
-                timeline_list.append(campaign["timeline"])
-        return {
-            "cycles_started": cycles_started,
-            "monthly_reports_sent": monthly_reports_sent,
-            "cycle_reports_sent": cycle_reports_sent,
-            "yearly_reports_sent": yearly_reports_sent,
-            "timeline_list": timeline_list,
-        }
-
-    def _get_customer_type_count(self):
-        """Customer_type_count.
-
-        Returns:
-            dict: dict created with customer stats
-        """
         customers = customer_service.get_list()
         federal_customers = 0
         state_customers = 0
@@ -153,14 +87,38 @@ class SystemReportsView(APIView):
                     tribal_customers += 1
                 if customer["customer_type"] == "Private":
                     private_customers += 1
-        return {
+
+        gov_group_stats = get_gov_group_stats()
+        gov_group_stats["fed_stats"]["customer_count"] = federal_customers
+        gov_group_stats["state_stats"]["customer_count"] = state_customers
+        gov_group_stats["local_stats"]["customer_count"] = local_customers
+        gov_group_stats["tribal_stats"]["customer_count"] = tribal_customers
+        gov_group_stats["private_stats"]["customer_count"] = private_customers
+
+        if "average" in consolidated_stats["clicked"]:
+            avgTimeToClick = format_timedelta(consolidated_stats["clicked"]["average"])
+        else:
+            avgTimeToClick = None
+
+        context = {
             "customers_enrolled": len(customers),
-            "federal_customers": federal_customers,
-            "state_customers": state_customers,
-            "local_customers": local_customers,
-            "tribal_customers": tribal_customers,
-            "private_customers": private_customers,
+            "monthly_reports_sent": monthly_reports_sent,
+            "cycle_reports_sent": cycle_reports_sent,
+            "yearly_reports_sent": yearly_reports_sent,
+            "federal_stats": gov_group_stats["fed_stats"],
+            "state_stats": gov_group_stats["state_stats"],
+            "local_stats": gov_group_stats["local_stats"],
+            "tribal_territorial_stats": gov_group_stats["tribal_stats"],
+            "critical_infrastructure_private_stats": gov_group_stats["private_stats"],
+            "click_rate_across_all_customers": consolidated_stats["ratios"][
+                "clicked_ratio"
+            ],
+            "average_time_to_click_all_customers": consolidated_stats["clicked"],
+            "avgTimeToClick": avgTimeToClick
+            # ["clicked"]["average"],
         }
+
+        return Response(context, status=status.HTTP_202_ACCEPTED)
 
 
 class SubsriptionReportsListView(APIView):
