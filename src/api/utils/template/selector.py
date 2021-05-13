bd2df8d02a93f32164ee9df6a350cbdca4cd042d
Zachary Wilson <zachary.wilson@inl.gov>
2021-05-13 09:04:35 -0600

    fix template grouping

--- src/api/utils/template/selector.py
+++ src/api/utils/template/selector.py
@@ -1,21 +1,30 @@
 """Template selector utils."""
 # Standard Python Libraries
-import itertools
 import random
 
 
 def select_templates(templates: list, count: int = 5) -> dict:
     """Group Templates by score."""
-    result = {}
-    for k, g in itertools.groupby(templates, group_template):
-        selected = list(g)
-        sample_size = count if len(selected) > count else len(selected)
-        result[k] = random.sample(selected, sample_size)
-    return result
+    grouped_templates = group_templates(templates)
+    for k, v in grouped_templates.items():
+        sample_size = count if len(v) > count else len(v)
+        grouped_templates[k] = random.sample(v, sample_size)
+    return grouped_templates
+
+
+def group_templates(templates):
+    """Group all templates by deception score."""
+    groups = {}
+    for template in templates:
+        group = group_template(template)
+        if not groups.get(group):
+            groups[group] = []
+        groups[group].append(template)
+    return groups
 
 
 def group_template(template: dict) -> str:
-    """Return group for a template."""
+    """Return group for a template based on deception score.."""
     levels = {"high": 5, "moderate": 3, "low": 0}
     score = template["deception_score"]
     if score < levels["moderate"]:
