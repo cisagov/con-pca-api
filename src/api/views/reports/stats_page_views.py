c9ddd689ccfd7cc94b614be45aad0d5309b9897e
Zachary Wilson <zachary.wilson@inl.gov>
2021-07-09 12:37:40 -0600

    Simple asn org stats

--- src/api/views/reports/stats_page_views.py
+++ src/api/views/reports/stats_page_views.py
@@ -5,13 +5,18 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 # cisagov Libraries
-from api.services import SubscriptionService
+from api.services import CampaignService, SubscriptionService
 from api.utils.generic import format_timedelta
 from api.utils.reports import is_nonhuman_request
-from api.utils.stats import deception_stats_to_graph_format, get_subscription_cycle
+from api.utils.stats import (
+    deception_stats_to_graph_format,
+    get_asn_org_stats,
+    get_subscription_cycle,
+)
 from api.views.reports.cycle_report_views import get_cycle_stats
 
 subscription_service = SubscriptionService()
+campaign_service = CampaignService()
 
 
 class CycleStatusView(APIView):
@@ -20,12 +25,21 @@ class CycleStatusView(APIView):
     def get(self, request, subscription_uuid, cycle_uuid):
         """Get."""
         # Get targeted subscription and associated customer data
-        subscription = subscription_service.get(subscription_uuid)
+        subscription = subscription_service.get(
+            subscription_uuid, fields=["subscription_uuid", "cycles"]
+        )
         cycle = get_subscription_cycle(subscription, cycle_uuid)
+        subscription["campaigns"] = campaign_service.get_list(
+            parameters={"cycle_uuid": cycle_uuid}
+        )
 
         # Get statistics for the specified subscription during the specified cycle
         stats = get_cycle_stats(subscription, cycle, is_nonhuman_request(request))
         # get_template_details(subscription_stats["campaign_results"])
+        timeline = []
+        for campaign in subscription["campaigns"]:
+            timeline.extend(campaign["timeline"])
+        asn_stats = get_asn_org_stats(timeline)
 
         context = {
             "avg_time_to_first_click": format_timedelta(
@@ -40,6 +54,7 @@ class CycleStatusView(APIView):
             "aggregate_stats": stats["stats_all"],
             # "stats": subscription_stats,
             "levels": deception_stats_to_graph_format(stats),
+            "asn_stats": asn_stats,
         }
 
         return Response(context, status=status.HTTP_202_ACCEPTED)
