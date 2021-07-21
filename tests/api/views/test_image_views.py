ff697aed3bf58aaf2196e7fe1ed265c694813845
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-07-21 18:33:57 -0500

    unit test fix - image_view

--- tests/api/views/test_image_views.py
+++ tests/api/views/test_image_views.py
@@ -1,41 +1,35 @@
 """Image View Tests."""
 # Standard Python Libraries
+import base64
 from mimetypes import guess_type
-from unittest import mock
 
 # Third-Party Libraries
 from django.core.files.uploadedfile import InMemoryUploadedFile
-from faker import Faker
 import pytest
-
-fake = Faker()
 
 
 @pytest.mark.django_db
 def test_image_view_post(client):
     """Test Image View Post."""
-    with mock.patch(
-        "api.utils.aws_utils.S3.upload_fileobj_image"
-    ) as mock_upload, mock.patch("boto3.client"):
-        uuid = fake.uuid4()
-        mock_upload.return_value = uuid, "testbucket", "testurl"
+    name = "src/static/img/cisa_logo.png"
+    with open(name, "rb") as f:
+        f.seek(0)
+        f.read()
+        size = f.tell()
+        f.seek(0)
+        content_type, charset = guess_type(name)
+        uf = InMemoryUploadedFile(
+            file=f,
+            name=name,
+            field_name=None,
+            content_type=content_type,
+            size=size,
+            charset=charset,
+        )
 
-        name = "src/static/img/cisa_logo.png"
-        with open(name, "rb") as f:
-            f.seek(0)
-            f.read()
-            size = f.tell()
-            f.seek(0)
-            content_type, charset = guess_type(name)
-            uf = InMemoryUploadedFile(
-                file=f,
-                name=name,
-                field_name=None,
-                content_type=content_type,
-                size=size,
-                charset=charset,
-            )
-
-            resp = client.post("/api/v1/imageupload/", data={"file": uf})
-        assert resp.status_code == 201
-        assert mock_upload.called
+        resp = client.post("/api/v1/imageupload/", data={"file": uf})
+    assert resp.status_code == 201
+    assert (
+        f"data:image/jpeg;base64,{base64.b64encode(base64.b64decode(uf))}"
+        == resp.json()["imageUrl"]
+    )
