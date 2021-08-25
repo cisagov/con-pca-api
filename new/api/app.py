5b1bf22163c09805db8b5bf0e12321028b8321d2
Zachary Wilson <zachary.wilson@inl.gov>
2021-08-25 08:26:46 -0600

    bootstrap change

--- new/api/app.py
+++ new/api/app.py
@@ -1,13 +1,8 @@
 """Flask app."""
 # Third-Party Libraries
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from flask_cors import CORS
 
 app = Flask(__name__, template_folder="templates")
 app.url_map.strict_slashes = False
 CORS(app)
-Bootstrap(app)
-
-
-print("test")
