d5c131ca8736f4cffb4f4e3a69d606b041d2879b
Israel Bentley <50339477+izzy64@users.noreply.github.com>
2022-06-16 12:19:04 -0600

    precommit workaround testing

--- src/api/views/sending_profile_views.py
+++ src/api/views/sending_profile_views.py
@@ -35,8 +35,12 @@ class SendingProfilesView(MethodView):
         for subscription in subscription_manager.all():
             if subscription["primary_contact"]["email"] not in unique_emails:
                 unique_emails.append(subscription["primary_contact"]["email"])
-                Notification("domain_added_notice",subscription=subscription,cycle=cycle_manager.get(filter_data={"active": True})
-                             ,new_domain=request.json['from_address'].split("@")[1]).send()
+                Notification(
+                    "domain_added_notice",
+                    subscription=subscription,
+                    cycle=cycle_manager.get(filter_data={"active": True}),
+                    new_domain=request.json["from_address"].split("@")[1],
+                ).send()
         return jsonify(sending_profile_manager.save(request.json))
 
 
