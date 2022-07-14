e29f9e2ed22b2b64abc817486866ba0f5b53f33b
Israel Bentley <50339477+izzy64@users.noreply.github.com>
2022-07-14 09:10:24 -0600

    oops function names should match

--- tests/reports_aggregate_test.py
+++ tests/reports_aggregate_test.py
@@ -4,18 +4,18 @@
 import pytest
 
 
-class TestSectorIndustries:
+class TestReportsAggregate:
     """Test case for reports aggregate related views."""
 
-    def test_sector_industrys_view(self, client):
+    def test_reports_aggregate_view(self, client):
         """Test the reports aggregates view."""
         resp = client.get("/api/reports/aggregate/")
         assert resp.status_code == 200
 
-        self.check_sector_industry_properties(resp.json)
+        self.check_reports_aggregate_properties(resp.json)
 
     @staticmethod
-    def check_sector_industry_properties(reportsaggregate):
+    def check_reports_aggregate_properties(reportsaggregate):
         """Check reportsaggregate object for expected properties."""
         if not isinstance(reportsaggregate, dict):
             pytest.fail("expected a dict")
