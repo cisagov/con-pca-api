76b4ddb097727f3a4ae623aa2f5f60a51dedbc00
Israel Bentley <50339477+izzy64@users.noreply.github.com>
2022-08-31 14:14:39 -0600

    Accounting for the fact that the archived bool may not exist

--- src/utils/reports.py
+++ src/utils/reports.py
@@ -328,7 +328,7 @@ def get_reports_sent():
     }
 
     pipeline = [
-        {"$match": {"archived": False}},
+        {"$match": {"archived": {"$ne": True}}},
         {"$unwind": "$notification_history"},
         {"$group": {"_id": "$notification_history.message_type", "count": {"$sum": 1}}},
         {
@@ -400,7 +400,7 @@ def get_sector_industry_report():
             for c in customer_manager.all(
                 params={
                     "customer_type": stat.split("_")[0].capitalize(),
-                    "archived": False,
+                    "archived": {"$ne": True},
                 },
                 fields=["_id"],
             )
@@ -408,7 +408,7 @@ def get_sector_industry_report():
         subscriptions = [
             s["_id"]
             for s in subscription_manager.all(
-                params={"customer_id": {"$in": customers}, "archived": False},
+                params={"customer_id": {"$in": customers}, "archived": {"$ne": True}},
                 fields=["_id"],
             )
         ]
