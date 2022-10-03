85cb05a822fc2346361e2029062b2d11e723bed2
Israel Bentley <israel.bentley@icloud.com>
2022-10-03 10:35:08 -0600

    stakeholder shortname implementation

--- src/api/views/customer_views.py
+++ src/api/views/customer_views.py
@@ -6,6 +6,7 @@ from flask.views import MethodView
 # cisagov Libraries
 from api.manager import CustomerManager, SubscriptionManager
 from utils.sectors import SECTORS
+from utils.subscriptions import create_subscription_name
 
 customer_manager = CustomerManager()
 subscription_manager = SubscriptionManager()
@@ -43,6 +44,11 @@ class CustomerView(MethodView):
         """Put."""
         customer_manager.update(document_id=customer_id, data=request.json)
         return jsonify({"success": True})
+
+    def post(self, customer_id):
+        """Post."""
+        customer = customer_manager.get(document_id=customer_id)
+        return create_subscription_name(customer)
 
     def delete(self, customer_id):
         """Delete."""
