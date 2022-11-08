4e45ca4248a9c7305ef968fa69369024afe1c450
Israel Bentley <israel.bentley@icloud.com>
2022-11-08 13:41:00 -0700

    set a gap between cooldown period and end cycle for non-continuous subs

--- src/utils/subscriptions.py
+++ src/utils/subscriptions.py
@@ -196,7 +196,7 @@ def get_initial_tasks(subscription, cycle):
             minutes=(cycle_minutes + subscription.get("buffer_time_minutes", 0))
         )
     else:
-        task_types["end_cycle"] = start_date + timedelta(minutes=(cycle_minutes))
+        task_types["end_cycle"] = start_date + timedelta(minutes=(cycle_minutes + 15))
 
     cycle_days = cycle_minutes / 60 / 24
     if cycle_days >= 30:
