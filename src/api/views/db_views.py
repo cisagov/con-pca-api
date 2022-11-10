eeccc88ba9e010f5b30b137b84ffb1da46d5e560
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2022-11-10 13:16:06 -0800

    add load and dump db views

---
+++ src/api/views/db_views.py
@@ -0,0 +1,49 @@
+"""Database Management Views."""
+# Standard Python Libraries
+import io
+
+# Third-Party Libraries
+from bson.json_util import dumps, loads
+from flask import jsonify, request, send_file
+from flask.views import MethodView
+
+# cisagov Libraries
+from api.config.environment import DB
+
+database = DB
+
+
+class DatabaseManagementView(MethodView):
+    """Dump and restore mongo data."""
+
+    def get(self):
+        """Dump mongo data."""
+        data_dump = {}
+        for coll in database.collection_names():
+            data_dump[coll] = [d for d in database[coll].find({})]
+
+        mem = io.BytesIO()
+        mem.write(dumps(data_dump).encode("utf-8"))
+        mem.seek(0)
+
+        return send_file(mem, mimetype="application/json")
+
+    def post(self):
+        """Restore mongo data."""
+        collections = loads(request.data)
+
+        if not isinstance(collections, dict):
+            return jsonify({"error": "Invalid data format."}), 400
+
+        for coll_name in collections.keys():
+            # drop any potentially existing collection
+            database[coll_name].drop()
+
+            # drop in new collection
+            coll_values = []
+            for d in collections[coll_name]:
+                coll_values.append(d)
+
+            database[coll_name].insert_many(coll_values) if coll_values else None
+
+        return jsonify({"message": "Mongo data successfully restored."})
