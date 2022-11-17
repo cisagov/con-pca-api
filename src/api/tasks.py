929c0874dd8a4c38be010ca6d1b51eda7667f49f
Israel Bentley <israel.bentley@icloud.com>
2022-11-17 13:47:52 -0700

    keep a clone of subscription tasks and update it every time the subscription is processed

--- src/api/tasks.py
+++ src/api/tasks.py
@@ -106,6 +106,13 @@ def process_subscription(subscription):
         document_id=subscription["_id"], data={"processing": False}, update=False
     )
 
+    cycle_manager.update(
+        document_id=cycle["_id"],
+        data={
+            "tasks": subscription.get("tasks"),
+        },
+    )
+
 
 def update_task(subscription_id, task):
     """Update subscription task."""
