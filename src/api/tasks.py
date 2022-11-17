863c5405ba45d3113ffd45901c0d27c1b4810735
Israel Bentley <israel.bentley@icloud.com>
2022-11-17 14:20:22 -0700

    update the correct cycle if a new one has just started

--- src/api/tasks.py
+++ src/api/tasks.py
@@ -106,6 +106,12 @@ def process_subscription(subscription):
         document_id=subscription["_id"], data={"processing": False}, update=False
     )
 
+    cycle = cycle_manager.get(
+        filter_data={
+            "subscription_id": str(subscription["_id"]),
+            "active": True,
+        }
+    )
     cycle_manager.update(
         document_id=cycle["_id"],
         data={
