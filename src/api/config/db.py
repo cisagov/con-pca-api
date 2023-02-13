fe68e78bd4ca4e603058fcf9bdeba876274adcb1
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-02-13 09:45:14 -0800

    feature: code cleanup after docdb removal

--- src/api/config/db.py
+++ src/api/config/db.py
@@ -6,24 +6,6 @@ import os
 from pymongo import MongoClient
 
 
-def get_connection_string(db_user, db_pw, db_host, db_port):
-    """Get connection string for connecting to MongoDB."""
-    if os.environ.get("MONGO_TYPE", "MONGO") == "DOCUMENTDB":
-        fmt = "mongodb://{}:{}@{}:{}/?tls=true&tlsCAFile=static/rds-combined-ca-bundle.pem&retryWrites=false"
-    else:
-        fmt = "mongodb://{}:{}@{}:{}/"
-    return fmt.format(db_user, db_pw, db_host, db_port)
-
-
 def get_db():
     """Get database client."""
-    if os.environ.get("MONGO_TYPE") == "ATLAS" and os.environ.get("MONGO_CLUSTER_URI"):
-        return MongoClient(os.environ["MONGO_CLUSTER_URI"], tz_aware=True).pca
-
-    conn_str = get_connection_string(
-        os.environ.get("DB_USER"),
-        os.environ.get("DB_PW"),
-        os.environ.get("DB_HOST"),
-        os.environ.get("DB_PORT"),
-    )
-    return MongoClient(conn_str, tz_aware=True).pca
+    return MongoClient(os.environ["MONGO_CLUSTER_URI"], tz_aware=True).pca
